3d72853edbcf0d880ca236d3b74b9cf11a3ed8f6
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-03-06 18:56:12 -0300

    stats - req02

---
+++ challenges/desafio2.sql
@@ -0,0 +1,14 @@
+SELECT 
+    (SELECT 
+            COUNT(*)
+        FROM
+            SpotifyClone.musics) AS cancoes,
+    (SELECT 
+            COUNT(*)
+        FROM
+            SpotifyClone.artists) AS artistas,
+    (SELECT 
+            COUNT(*)
+        FROM
+            SpotifyClone.albums) AS albuns;
+            
